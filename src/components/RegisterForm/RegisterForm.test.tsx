dea93e5d56e30e439c67291bf42902119bc08b38
Adria <adriamartinez94@gmail.com>
2022-11-23 15:53:25 +0100

    Add test of userRegister

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../mocks/renderWithProviders";
 import { RegisterForm } from "./RegisterForm";
 
---
+++ src/hooks/useUser/useUser.test.ts
@@ -0,0 +1,51 @@
+import { renderHook } from "@testing-library/react";
+import ProviderWrapper from "../../mocks/providerWrapper";
+import { store } from "../../redux/store";
+import { UserRegisterData } from "../../types";
+import useUser from "./useUser";
+
+const dispatch = jest.spyOn(store, "dispatch");
+
+describe("Given the useUser hook", () => {
+  describe("When it is invoked with method userRegister with username 'Mario' alias '@mario' password 123 and email 'mario@mario.com'", () => {
+    test("Then it should be call the dispatch to open sucess alert", async () => {
+      const {
+        result: {
+          current: { registerUser },
+        },
+      } = renderHook(() => useUser(), { wrapper: ProviderWrapper });
+
+      const registerData: UserRegisterData = {
+        username: "Mario",
+        alias: "@mario",
+        email: "mario@mario.com",
+        password: "123",
+      };
+
+      await registerUser(registerData);
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  describe("When it is invoked with an user that exists", () => {
+    test("Then it should call the dispatch", async () => {
+      const {
+        result: {
+          current: { registerUser },
+        },
+      } = renderHook(() => useUser(), { wrapper: ProviderWrapper });
+
+      const registerData: UserRegisterData = {
+        alias: "",
+        password: "",
+        email: "",
+        username: "Chals",
+      };
+
+      await registerUser(registerData);
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+});
