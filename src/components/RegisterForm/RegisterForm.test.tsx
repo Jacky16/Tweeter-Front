f88dcae884dd4cccca4bb5269371817d32ad4377
Adria <adriamartinez94@gmail.com>
2022-11-24 14:41:12 +0100

    Adapt test of RegisterForm with register implementation

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,6 +1,14 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from "../../mocks/renderWithProviders";
 import { RegisterForm } from "./RegisterForm";
+
+const mockRegisterUser = jest.fn();
+jest.mock("../../hooks/useUser/useUser", () => {
+  return () => ({
+    registerUser: mockRegisterUser,
+  });
+});
 
 describe("Given the Register form component", () => {
   describe("When it's renderer", () => {
@@ -28,6 +36,36 @@ describe("Given the Register form component", () => {
       expect(inputUsername).toBeInTheDocument();
       expect(inputPassword).toBeInTheDocument();
       expect(inputAlias).toBeInTheDocument();
+    });
+  });
+  describe("When the user click the Sign up button", () => {
+    test("Then userLogin should be called", async () => {
+      const labelEmail = "Email";
+      const labelPassword = "password";
+      const labelAlias = "alias";
+      const labelUsername = "username";
+
+      const nameButton = "Sign up";
+
+      renderWithProviders(<RegisterForm />);
+
+      const buttonRegister = screen.getByRole("button", {
+        name: nameButton,
+      });
+
+      const inputEmail = screen.getByRole("textbox", { name: labelEmail });
+      const inputPassword = screen.getByLabelText(labelPassword);
+      const inputAlias = screen.getByLabelText(labelAlias);
+      const inputUsername = screen.getByLabelText(labelUsername);
+
+      await userEvent.type(inputEmail, "mario@gmail.com");
+      await userEvent.type(inputPassword, "123");
+      await userEvent.type(inputAlias, "Mario");
+      await userEvent.type(inputUsername, "@mario");
+
+      await userEvent.click(buttonRegister);
+
+      expect(mockRegisterUser).toBeCalled();
     });
   });
 });
