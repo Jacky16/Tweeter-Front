6ccb6cf15ca617c440150e775002a11a420f16e8
Adria <adriamartinez94@gmail.com>
2022-11-24 13:48:47 +0100

    Add register page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../../pages/LoginPage/LoginPage";
+import RegisterPage from "../../pages/RegisterPage/RegisterPage";
 
 const App = () => {
   return (
     <Container maxWidth={"sm"}>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Container>
   );
