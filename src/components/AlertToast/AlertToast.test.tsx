b8209a6bf439a6eb96fe3fd41e48bf5494114fe1
Adria <adriamartinez94@gmail.com>
2022-11-25 22:11:45 +0100

    Add new edge case on alertToast test

--- src/components/AlertToast/AlertToast.test.tsx
+++ src/components/AlertToast/AlertToast.test.tsx
@@ -1,6 +1,15 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../mocks/renderWithProviders";
+import { closeAlertActionCreator } from "../../redux/UiSlice/UiSlice";
 import AlertToast from "./AlertToast";
+
+const mockDispatch = jest.fn();
+
+jest.mock("../../redux/hooks", () => ({
+  ...jest.requireActual("../../redux/hooks"),
+  useAppDispatch: () => mockDispatch,
+}));
+jest.useFakeTimers();
 
 describe("Given the AlertToast component", () => {
   describe("When it's rendered", () => {
@@ -19,6 +28,24 @@ describe("Given the AlertToast component", () => {
 
       expect(alertToast).toBeInTheDocument();
       expect(textAlert).toBeInTheDocument();
+    });
+  });
+  describe("When 3 seconds have passed", () => {
+    test("Then it should close the alert", () => {
+      jest.useFakeTimers();
+      const expectedMessage = "Success";
+
+      renderWithProviders(
+        <AlertToast
+          severity={"error"}
+          message={expectedMessage}
+          isOpen={true}
+        />
+      );
+
+      jest.advanceTimersByTime(3000);
+
+      expect(mockDispatch).toHaveBeenCalledWith(closeAlertActionCreator());
     });
   });
 });
