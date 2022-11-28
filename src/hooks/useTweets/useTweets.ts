88d61572f70e018276b49b06a78d4e3cc5448cb6
Adria <adriamartinez94@gmail.com>
2022-11-28 21:27:02 +0100

    Add getOneTweet on useTweets

--- src/hooks/useTweets/useTweets.ts
+++ src/hooks/useTweets/useTweets.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { useCallback } from "react";
 import requestsUrl from "../../config/requestsUrl";
 import { useAppDispatch } from "../../redux/hooks";
-import { addTweetsActionCreator } from "../../redux/tweetsSlice/tweetsSlice";
+import {
+  addTweetsActionCreator,
+  loadTweetActionCreator,
+} from "../../redux/tweetsSlice/tweetsSlice";
 import {
   closeIsLoadingActionCreator,
   loadPaginationActionCreator,
@@ -45,7 +48,33 @@ const useTweets = () => {
     [dispatch]
   );
 
-  return { getTweets };
-};
+  const getOneTweet = async (token: string, idTweet: string) => {
+    dispatch(openIsLoadingActionCreator());
+    try {
+      const response = await axios.get(
+        `${requestsUrl.getOneTweet}/${idTweet}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const { tweet } = await response.data;
 
+      dispatch(closeIsLoadingActionCreator());
+      dispatch(loadTweetActionCreator(tweet));
+    } catch (error: unknown) {
+      dispatch(
+        openAlertActionCreator({
+          message: "Error to load tweet",
+          severity: "error",
+          isOpen: true,
+        })
+      );
+      dispatch(closeIsLoadingActionCreator());
+    }
+  };
+
+  return { getTweets, getOneTweet };
+};
 export default useTweets;
