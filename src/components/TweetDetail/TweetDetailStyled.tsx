5892488e14edce2b69ee76a7abea455c772b4d9e
Adria <adriamartinez94@gmail.com>
2022-11-29 16:14:38 +0100

    Add Tweet detail component

---
+++ src/components/TweetDetail/TweetDetailStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TweetDetailStyled = styled.article`
+  img {
+    object-fit: cover;
+    border-radius: 10px;
+  }
+`;
+
+export default TweetDetailStyled;
