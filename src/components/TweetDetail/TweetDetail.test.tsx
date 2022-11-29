37af047002208e18a18d2f637ca216b7b928d5c5
Adria <adriamartinez94@gmail.com>
2022-11-29 16:43:49 +0100

    Add test of tweet detail component

---
+++ src/components/TweetDetail/TweetDetail.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { getTweet } from "../../factory/tweetsFactory";
+import TweetDetail from "./TweetDetail";
+
+describe("Given the TweetDetail component", () => {
+  describe("When it's rendered with a tweet", () => {
+    test("Then it should show the category of tweet and his description", () => {
+      const tweet = getTweet();
+      const expectedCategory = "comedy";
+
+      render(<TweetDetail tweet={tweet} />);
+
+      const category = screen.getByText(expectedCategory);
+      const description = screen.getByText(tweet.description as string);
+
+      expect(category).toBeInTheDocument();
+      expect(description).toBeInTheDocument();
+    });
+  });
+});
