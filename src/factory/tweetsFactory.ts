a93e7e3393cc6cdd80c05185132852dbf9d627cc
Adria <adriamartinez94@gmail.com>
2022-12-01 13:38:25 +0100

    Create tweetApi factories

--- src/factory/tweetsFactory.ts
+++ src/factory/tweetsFactory.ts
@@ -1,4 +1,4 @@
-import { Tweet } from "../types";
+import { Tweet, TweetApi } from "../types";
 import { faker } from "@faker-js/faker";
 import { Factory } from "fishery";
 
@@ -12,6 +12,22 @@ const tweetFactory = Factory.define<Tweet>(() => ({
   dateOfCreation: faker.date.past().toString(),
 }));
 
+const tweetApiFactory = Factory.define<TweetApi>(() => ({
+  id: faker.database.mongodbObjectId(),
+  description: faker.lorem.sentence(),
+  category: "comedy",
+  image: faker.image.imageUrl(),
+  dateOfCreation: faker.date.past().toString(),
+  author: {
+    username: faker.name.firstName(),
+    alias: faker.name.lastName(),
+  },
+}));
+
 export const getTweets = (size: number) => tweetFactory.buildList(size);
 
 export const getTweet = (tweet?: Tweet) => tweetFactory.build(tweet);
+
+export const getTweetsApi = (size: number) => tweetApiFactory.buildList(size);
+
+export const getTweetApi = (tweet?: TweetApi) => tweetApiFactory.build(tweet);
