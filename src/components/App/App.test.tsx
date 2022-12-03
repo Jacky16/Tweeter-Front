e7d4b6e3f45e3aa09757e07fc307d89b95a33b80
Adria <adriamartinez94@gmail.com>
2022-12-03 21:57:14 +0100

    Add tests of filter on app tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -3,28 +3,30 @@ import { screen } from "@testing-library/react";
 import mockStore from "../../mocks/store/mockStore";
 import App from "./App";
 import mockUserLogged from "../../mocks/states/mockUserLogged";
+import { UiState } from "../../redux/types";
+
+const mockLoadTweetsByCategory = jest.fn();
+const mockGetTweetsByCategory = jest.fn();
+const mockGetTweets = jest.fn();
+const mockLoadTweets = jest.fn();
+
+jest.mock("../../hooks/useTweets/useTweets", () => {
+  return () => ({
+    loadTweetsByCategory: mockLoadTweetsByCategory,
+    getTweetsByCategory: mockGetTweetsByCategory,
+    getTweets: mockGetTweets,
+    loadTweets: mockLoadTweets,
+  });
+});
 
 describe("Given an App component", () => {
   describe("When user is logged and stay in the login page", () => {
     describe("And go to home page", () => {
-      test("Then it should show the 'load more' button", () => {
+      test("Then it should show the 'Account settings' button", () => {
         const store = mockStore({ userPreloadState: mockUserLogged });
+
         const initialEntries = ["/home"];
-        const nameButton = "Load more";
-
-        renderWithProviders(<App />, { store, initialEntries });
-
-        const button = screen.getByRole("button", { name: nameButton });
-
-        expect(button).toBeInTheDocument();
-      });
-    });
-
-    describe("When user is not logged and go to home page", () => {
-      test("Then it should show the 'Sign in' button", () => {
-        const store = mockStore();
-        const initialEntries = ["/home"];
-        const nameButton = "Sign in";
+        const nameButton = "Account settings";
 
         renderWithProviders(<App />, { store, initialEntries });
 
@@ -35,19 +37,97 @@ describe("Given an App component", () => {
     });
   });
 
-  describe("When user is logged in home page", () => {
-    describe("And go to login page", () => {
-      test("Then it should return to home page and shoud show the 'load more' button", () => {
-        const store = mockStore({ userPreloadState: mockUserLogged });
-        const initialEntries = ["/"];
-        const nameButton = "Load more";
-
-        renderWithProviders(<App />, { store, initialEntries });
-
-        const button = screen.getByRole("button", { name: nameButton });
-
-        expect(button).toBeInTheDocument();
+  describe("When user is logged and category filter is science and current page is 2", () => {
+    test("Then loadTweetByCategory should be called", () => {
+      const store = mockStore({
+        userPreloadState: mockUserLogged,
+        uiPreloadState: {
+          categoryFilter: "science",
+          alert: {
+            isOpen: false,
+          },
+          pagination: {
+            currentPage: 2,
+          },
+        } as UiState,
       });
+      renderWithProviders(<App />, { store });
+
+      expect(mockGetTweetsByCategory).toBeCalled();
+    });
+  });
+
+  describe("When user is logged and category filter is all and current page is 2", () => {
+    test("Then getTweets should be called", () => {
+      const store = mockStore({
+        userPreloadState: mockUserLogged,
+        uiPreloadState: {
+          categoryFilter: "all",
+          alert: {
+            isOpen: false,
+          },
+          pagination: {
+            currentPage: 2,
+          },
+        } as UiState,
+      });
+      renderWithProviders(<App />, { store });
+
+      expect(mockGetTweets).toBeCalled();
+    });
+  });
+
+  describe("When user is logged and category filter is all and current page is 1", () => {
+    test("Then loadTweetsByCategory should be called", () => {
+      const store = mockStore({
+        userPreloadState: mockUserLogged,
+        uiPreloadState: {
+          categoryFilter: "all",
+          alert: {
+            isOpen: false,
+          },
+          pagination: {
+            currentPage: 1,
+          },
+        } as UiState,
+      });
+      renderWithProviders(<App />, { store });
+
+      expect(mockLoadTweets).toBeCalled();
+    });
+  });
+
+  describe("When user is logged and category filter is science and current page is 1", () => {
+    test("Then loadTweetsByCategory should be called", () => {
+      const store = mockStore({
+        userPreloadState: mockUserLogged,
+        uiPreloadState: {
+          categoryFilter: "science",
+          alert: {
+            isOpen: false,
+          },
+          pagination: {
+            currentPage: 1,
+          },
+        } as UiState,
+      });
+      renderWithProviders(<App />, { store });
+
+      expect(mockLoadTweetsByCategory).toBeCalled();
+    });
+  });
+
+  describe("When user is not logged and go to home page", () => {
+    test("Then it should show the 'Sign in' button", () => {
+      const store = mockStore();
+      const initialEntries = ["/home"];
+      const nameButton = "Sign in";
+
+      renderWithProviders(<App />, { store, initialEntries });
+
+      const button = screen.getByRole("button", { name: nameButton });
+
+      expect(button).toBeInTheDocument();
     });
   });
 });
