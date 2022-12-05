af606a36e934a701d59c2c850861f927ab304003
Adria <adriamartinez94@gmail.com>
2022-12-05 22:08:33 +0100

    Change category colors

---
+++ src/styles/categoryColors.tsx
@@ -0,0 +1,9 @@
+const categoryColors = {
+  comedy: "#892DFF",
+  political: "#D54000",
+  sports: "#D3000B",
+  entertainment: "#D00095",
+  science: "#263CFF",
+};
+
+export default categoryColors;
